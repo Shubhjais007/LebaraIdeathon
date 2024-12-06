8ec808829a732acb3f812d7b598b03ddbe623b15
Dwip Kumar Paul <dwip.kumar-ext@lebara.com>
2024-12-06 16:32:30 +0000

    UI initial commit

---
+++ ClientApp/src/App.tsx
@@ -0,0 +1,20 @@
+// import { Typography } from '@mui/material';
+// import { BrowserRouter, Route, Routes } from 'react-router';
+import Index from './routes';
+
+function App() {
+
+  return (
+    <>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<></>} />
+        </Routes>
+      </BrowserRouter>
+      <Typography variant='h4'>New App</Typography> */}
+      <Index />
+    </>
+  )
+}
+
+export default App
